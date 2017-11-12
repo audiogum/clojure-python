608613b8dedcd85a7bd894647a6777a08a369f62
Steve Robbins <sr@audiogum.com>
2017-11-12 08:57:06 +0000

    update versions

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject clojure-python "0.4.1"
+(defproject clojure-python "0.4.2"
   :description "Improve seamlessness of Clojure Jython interop."
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.python/jython-standalone "2.5.3"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.python/jython-standalone "2.7.1"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:dependencies [[midje "1.4.0"]]}}
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]}}
   :plugins [[lein-midje "2.0.0"]])
